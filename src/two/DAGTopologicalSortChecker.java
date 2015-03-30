f455ad464f8c23ecceb318534a91e0f95d127672
unknown <vincent1430@DB21-MTLAB-PC20.iiitd.edu.in>
2015-03-30 17:11:52 +0530

    Added the TopologicalSortChecker

---
+++ src/two/DAGTopologicalSortChecker.java
@@ -0,0 +1,24 @@
+package two;
+
+import edu.princeton.cs.algs4.Graph;
+
+public class DAGTopologicalSortChecker {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+	
+	public static boolean isCorrectTopologicalSort(Graph G,int[] order){
+		// The approach will be check for each edge v->w, v should come before w in order[].
+		// I can think of O(n2) method for it.
+		// Take the all the edges from G.adj() method and then check in order[]
+		
+		// http://cstheory.stackexchange.com/questions/4414/testing-identifying-a-topological-sorting
+		// All the edges need to be considered.  Ω(E) is the lowerbound.
+		return false;
+	}
+
+}
