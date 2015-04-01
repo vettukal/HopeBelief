6fec34aeebe7b0f7e2b52f89db1f05268e7c754b
unknown <vincent1430@DB21-MTLAB-PC20.iiitd.edu.in>
2015-04-01 22:28:34 +0530

    Added the root of a graph.

---
+++ src/two/Ex_4_2_16.java
@@ -0,0 +1,19 @@
+package two;
+
+public class Ex_4_2_16{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * What happens if you run Kosaraju’s algorithm on a DAG?
+		 */
+		
+//		The number of components in a graph can vary from 1 to V.
+//		There are V components of a DAG. 
+//		Hence if Kosaraju is run on a DAG then in answer there will V components.
+
+	}
+
+}
---
+++ src/two/RootDAG.java
@@ -0,0 +1,40 @@
+package two;
+
+import java.util.LinkedList;
+
+import edu.princeton.cs.algs4.Graph;
+
+public class RootDAG {
+	/**
+	 * 
+	 * @param G=the graph for which you have to find roots. 
+	 * @return array of roots.
+	 * NOTE: There can be more than one roots in a DAG
+	 * 
+	 * See: http://cs.stackexchange.com/questions/9133/finding-a-source-of-a-directed-acyclic-graph-in-linear-time
+	 */
+	public static int[] getDigraphRoot(Graph G){
+		boolean[] check = new boolean[G.V()];
+		for(int v=0;v<G.V();v++){
+			for (int w : G.adj(v)) {
+				check[w] = true;
+			}
+		}
+		
+		int counter =0;
+		for (int i = 0; i < check.length; i++) {
+			if(check[i]==true){
+				++counter;
+			}
+		}
+		int[] ans= new int[counter];
+		counter = 0;
+		for (int i = 0; i < check.length; i++) {
+			if(check[i]){
+				ans[counter++] = i;
+			}
+		}
+		return ans;
+	}
+
+}
